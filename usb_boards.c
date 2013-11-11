893dab3e264e47daffc80b30eec0eeba15af3552
micges <micges@wp.pl>
2013-11-11 01:58:22 +0100

    add basic support for detecting 7i43 in usb mode

--- usb_boards.c
+++ usb_boards.c
@@ -29,24 +29,47 @@ void usb_boards_scan(board_access_t *access) {
     u8 dev_name[4];
 
     data = lbp_read_ctrl(LBP_CMD_READ_COOKIE);
-    if (data != LBP_COOKIE) {
-        printf("ERROR: no LBP remote device found on %s\n", access->dev_addr);
-        return;
-    }
-    dev_name[0] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME0);
-    dev_name[1] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME1);
-    dev_name[2] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME2);
-    dev_name[3] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME3);
+    if (data == LBP_COOKIE) {
+        dev_name[0] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME0);
+        dev_name[1] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME1);
+        dev_name[2] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME2);
+        dev_name[3] = lbp_read_ctrl(LBP_CMD_READ_DEV_NAME3);
 
-    if (strncmp(dev_name, "7I64", 4) == 0) {
-        board->type = BOARD_USB;
-        strcpy(board->dev_addr, access->dev_addr);
-        strncpy(board->llio.board_name, dev_name, 4);
-        board->llio.private = board;
-        board->llio.verbose = access->verbose;
+		if (strncmp(dev_name, "7I64", 4) == 0) {
+			board->type = BOARD_USB;
+			strcpy(board->dev_addr, access->dev_addr);
+			strncpy(board->llio.board_name, dev_name, 4);
+			board->llio.private = board;
+			board->llio.verbose = access->verbose;
 
-        boards_count++;
-    }
+			boards_count++;
+		}
+	} else {
+		cmd = '1';
+        lbp_send(&cmd, 1);
+        lbp_recv(&data, 1);
+        if ((data & 0x01) == 0) {  // found 7i43
+			board->type = BOARD_USB;
+			strcpy(board->dev_addr, access->dev_addr);
+			strncpy(board->llio.board_name, "7I43", 4);
+            board->llio.num_ioport_connectors = 2;
+            board->llio.pins_per_connector = 24;
+            board->llio.ioport_connector_name[0] = "P3";
+            board->llio.ioport_connector_name[1] = "P4";
+            cmd = '0';
+			lbp_send(&cmd, 1);
+            lbp_recv(&data, 1);
+            if (data & 0x01)
+				board->llio.fpga_part_number = "3s400tq144";
+			else 
+				board->llio.fpga_part_number = "3s200tq144";
+            board->llio.num_leds = 8;
+			board->llio.private = board;
+			board->llio.verbose = access->verbose;
+
+			boards_count++;
+        }
+	}
 }
 
 void usb_boards_release(board_access_t *access) {
