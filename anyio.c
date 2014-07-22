fbd32ea576f1c603d3f794aa98bd2a335e3a40c9
Michael Geszkiewicz <micges@wp.pl>
2014-07-22 23:59:01 +0200

    libanyio: support --reload and --reset only on ethernet cards
    Signed-off-by: Michael Geszkiewicz <micges@wp.pl>

--- anyio.c
+++ anyio.c
@@ -262,11 +262,21 @@ int anyio_dev_set_remote_ip(board_t *board, char *lbp16_set_ip_addr) {
 }
 
 int anyio_dev_reload(board_t *board, int fallback_flag) {
-    return eth_board_reload(board, fallback_flag);
+    if (board->type == BOARD_ETH) {
+        return eth_board_reload(board, fallback_flag);
+    } else {
+        printf("ERROR: FPGA reload only supported by ethernet cards.\n");
+        return -1;
+    }
 }
 
 int anyio_dev_reset(board_t *board) {
-    return eth_board_reset(board);
+    if (board->type == BOARD_ETH) {
+        return eth_board_reset(board);
+    } else {
+        printf("ERROR: FPGA reset only supported by ethernet cards.\n");
+        return -1;
+    }
 }
 
 void anyio_dev_print_hm2_info(board_t *board) {
